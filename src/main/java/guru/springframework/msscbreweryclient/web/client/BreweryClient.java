7908ebc9c07ae13534024c37d23de1ab813ef54b
Alessandro Arosio <alessandro.arosio1@gmail.com>
2019-10-12 20:01:19 +0100

    added post request to save a beer + junit test

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
@@ -29,5 +30,10 @@ public class BreweryClient {
 
   public BeerDto getBeerById(UUID uuid) {
     return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+  }
+
+  // the REST action will send back a URI - gives back a location in the header
+  public URI saveNewBeer(BeerDto beerDto) {
+    return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
   }
 }
--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * All the tests in this class will require the mssc-brewery application up and running.
+ */
 @SpringBootTest
 class BreweryClientTest {
 
@@ -21,6 +25,17 @@ class BreweryClientTest {
     BeerDto dto = breweryClient.getBeerById(UUID.randomUUID());
 
     assertNotNull(dto);
+  }
 
+  @Test
+  public void saveNewBeer() {
+    // given
+    BeerDto beerDto = BeerDto.builder().beerName("New beer").build();
+
+    URI uri = breweryClient.saveNewBeer(beerDto);
+
+    assertNotNull(uri);
+
+    System.out.println(uri.toString());
   }
 }
