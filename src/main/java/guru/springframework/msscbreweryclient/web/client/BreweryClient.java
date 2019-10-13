e99ba38eeef7f02dcb6897bdf61bb352342f8d69
Alessandro Arosio <alessandro.arosio1@gmail.com>
2019-10-13 12:05:56 +0100

    added the same rest template for customer, plus junit

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -15,6 +16,7 @@ import java.util.UUID;
 public class BreweryClient {
 
   public final String BEER_PATH_V1 = "/api/v1/beer/";
+  public final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
   private String apihost;
 
   private final RestTemplate restTemplate;
@@ -43,5 +45,25 @@ public class BreweryClient {
 
   public void deleteBeer(UUID uuid) {
     restTemplate.delete(apihost + BEER_PATH_V1 + uuid.toString());
+  }
+
+  // this isn't great, usually I'll do an interface, but for the example it'll work fine
+
+
+  public CustomerDto getCustomerById(UUID uuid) {
+    return restTemplate.getForObject(apihost + CUSTOMER_PATH_V1 + uuid.toString(), CustomerDto.class);
+  }
+
+  // the REST action will send back a URI - gives back a location in the header
+  public URI saveNewCustomer(CustomerDto customerDto) {
+    return restTemplate.postForLocation(apihost + CUSTOMER_PATH_V1, customerDto);
+  }
+
+  public void updateCustomer(UUID uuid, CustomerDto customerDto) {
+    restTemplate.put(apihost + CUSTOMER_PATH_V1 + uuid.toString(), customerDto );
+  }
+
+  public void deleteCustomer(UUID uuid) {
+    restTemplate.delete(apihost + CUSTOMER_PATH_V1 + uuid.toString());
   }
 }
--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -50,5 +51,39 @@ class BreweryClientTest {
   @Test
   public void testDeleteBeer() {
     breweryClient.deleteBeer(UUID.randomUUID());
+  }
+
+
+  @Test
+  void getCustomerById() {
+
+    CustomerDto dto = breweryClient.getCustomerById(UUID.randomUUID());
+
+    assertNotNull(dto);
+  }
+
+  @Test
+  public void saveNewCustomer() {
+    // given
+    CustomerDto customerDto = CustomerDto.builder().name("Alessandro").build();
+
+    URI uri = breweryClient.saveNewCustomer(customerDto);
+
+    assertNotNull(uri);
+
+    System.out.println(uri.toString());
+  }
+
+  @Test
+  public void updateCustomer() {
+    // given
+    CustomerDto customerDto =  CustomerDto.builder().name("Alessandro").build();
+
+    breweryClient.updateCustomer(UUID.randomUUID(), customerDto);
+  }
+
+  @Test
+  public void testDeleteCustomer() {
+    breweryClient.deleteCustomer(UUID.randomUUID());
   }
 }
